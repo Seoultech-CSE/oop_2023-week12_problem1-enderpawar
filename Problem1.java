4ef222901550fb2462c45b5e36c74ad4c649093f
enderpawar <71482488+enderpawar@users.noreply.github.com>
2023-05-26 14:45:23 +0900

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,34 +1,37 @@
 import java.util.*;
 
-public class week13_1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class Problem1 {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
 
-        // 필요한 자료형을 넣어주세요
-           ArrayList<Number> list = new ArrayList<Number>(); // ArrayList에 Number 자료형 사용
+scss
+Copy code
+    // ArrayList에 Integer와 Double 자료형을 사용합니다.
+    ArrayList<Number> list = new ArrayList<>();
+
     System.out.print("Enter five integers and five doubles: ");
 
-    // 정수 5개와 실수 5개 입력 받기
+    // 정수 5개와 실수 5개를 입력받아 ArrayList에 저장합니다.
     for (int i = 0; i < 5; i++) {
         int num = input.nextInt();
-        list.add(num); // 정수를 ArrayList에 추가
+        list.add(num);
     }
 
     for (int i = 0; i < 5; i++) {
         double num = input.nextDouble();
-        list.add(num); // 실수를 ArrayList에 추가
+        list.add(num);
     }
 
-    sort(list); // 리스트 정렬
+    sort(list);
 
-    // 정렬된 리스트 출력
+    // 정렬된 ArrayList를 출력합니다.
     for (Number num : list) {
         System.out.print(num + " ");
     }
 }
 
 public static void sort(ArrayList<Number> list) {
-    Collections.sort(list); // ArrayList 정렬
+    // ArrayList를 정렬합니다.
+    Collections.sort(list);
 }
-
 }
